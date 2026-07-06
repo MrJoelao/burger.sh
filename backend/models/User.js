bc1a2126253b3dacd35da0e2a8990f763fab9085
Joel Stephan <joel.stephan.email@gmail.com>
2026-07-06 16:10:15 +0200

    creato lo schema per utente

---
+++ backend/models/User.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+
+const addressSchema = new mongoose.Schema(
+  // entità dell'indirizzo
+  {
+    street: { type: String, trim: true },
+    city: { type: String, trim: true },
+    zip: { type: String, trim: true },
+  },
+  { _id: false }, // evita che mongodb aggiunga un id innecesario
+);
+
+const userSchema = new mongoose.Schema(
+  // entità completa di user
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    surname: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    passwordHash: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["customer", "manager", "admin"], // il suo ruolo, può variare tra cliente, manager e admin
+      required: true,
+    },
+    address: addressSchema,
+    managerStatus: {
+      type: String,
+      enum: ["pending", "approved"], // un manager deve essere "approvato" dall'admin
+    },
+  },
+  {
+    timestamps: true, // mongodb gestirà in automatico la data e ora di quando un utente verrà aggiunto/modificato
+  },
+);
+
+module.exports = mongoose.model("User", userSchema);
