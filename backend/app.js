6fca23dc7d0c5a185767ed67417d8f1b302b6053
Joel Stephan <joel.stephan.email@gmail.com>
2026-08-18 20:01:20 +0200

    aggiunti gli handler di errore e tolto vario boilerplate

--- backend/app.js
+++ backend/app.js
@@ -1,41 +1,32 @@
-var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var notFound = require('./middlewares/notFound');
+var errorHandler = require('./middlewares/errorHandler');
 
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.get('/api/health', function(req, res) {
+  res.status(200).json({
+    success: true,
+    message: 'API funzionante'
+  });
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+// middleware che gestisce le richieste che non corrispondenti a nessuna delle route definite.
+// deve essere registrata dopo tutte le route, altrimenti intercetterebbe ogni richiesta prima che possa
+// raggiungere il controller corretto.
+app.use(notFound);
+
+// middleware globale per la gestione degli errori. deve essere l’ultimo middleware perché deve poter ricevere
+// gli errori generati dalle route e dai middleware precedenti.
+app.use(errorHandler);
 
 module.exports = app;
