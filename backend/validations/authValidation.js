7dd85e761d25d208cedc3c0346158c6ae4677f72
Joel Stephan <joel.stephan.email@gmail.com>
2026-08-21 09:14:46 +0200

    ho dimenticato il cognome

--- backend/validations/authValidation.js
+++ backend/validations/authValidation.js
@@ -2,6 +2,7 @@ const Joi = require('joi');
 
 const registerSchema = Joi.object({
   name: Joi.string().min(2).required(),
+  surname: Joi.string().min(2).required(),
   email: Joi.string().email().required(),
   password: Joi.string().min(6).required(),
   role: Joi.string().valid('customer', 'manager').required()
